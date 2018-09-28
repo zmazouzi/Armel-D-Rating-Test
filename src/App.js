65d9b36cad014c77350ffb8113f34d9b1fe8d781
Armel <41400108+Armelabdelkbir@users.noreply.github.com>
2018-09-28 11:14:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import map from './Map';
+
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+              <div id='map-container'> <map></map> </div>
+
       </div>
     );
   }
